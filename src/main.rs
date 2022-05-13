ceed7f52ce79991aa650379285628c414d8585bb
Ifeoluwakolopin <areifeoluwa10@gmail.com>
2022-05-13 16:41:56 -0700

    added inline comments

--- src/main.rs
+++ src/main.rs
@@ -1,21 +1,32 @@
+// The args library allows us read input from the command line
 use std::env::args;
 
 fn main() {
+    // Store command-line arguments in a variable.
     let mut args = args();
 
+    // Unpacks the arguments using the nth iterator.
+    // unwrap() unwraps the value of the iterator and panics the app
+    //  in case of an error.
+    // Args are separated by a single space. Example: cargo run -- 1 + 2
     let first = args.nth(1).unwrap();
+    // Convert the String() to char.
     let operator = args.nth(0).unwrap().chars().next().unwrap();
     let second = args.nth(0).unwrap();
 
+    // Converts the strings to floats
     let first_number = first.parse::<f32>().unwrap();
     let second_number = second.parse::<f32>().unwrap();
 
+    // Operates on the numbers
     let result = operate(operator, first_number, second_number);
-    
+
     println!("{:?}", output(first_number, second_number, operator, result));
 
 }
 
+// Function takes in the numbers and the operators and operates on them
+// based on the input. Panics app if the operator is invalid.
 fn operate(operator: char, first_number: f32, second_number: f32) -> f32 {
     match operator {
         '+' => first_number + second_number,
@@ -26,6 +37,8 @@ fn operate(operator: char, first_number: f32, second_number: f32) -> f32 {
     }
 }
 
+// This takes the inputs and the result from the operator and format
+// them to be printed in the terminal.
 fn output(first_number: f32, second_number: f32, operator: char, result: f32) -> String {
     format!("{} {} {} = {}", first_number, operator, second_number, result)
 }
