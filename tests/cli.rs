3489deab87fc9b688409253dac11c01b76b84823
Ifeoluwakolopin <areifeoluwa10@gmail.com>
2022-05-30 08:53:38 -0700

    refactor test

--- tests/cli.rs
+++ tests/cli.rs
@@ -3,8 +3,8 @@ use assert_cmd::Command;
 #[test]
 fn runs() {
     let mut cmd = Command::cargo_bin("image_combiner").unwrap();
-    cmd.arg("images/1.jpeg")
-        .arg("images/2.jpeg")
-        .arg("images/output.jpeg");
+    cmd.arg("tests/images/1.jpeg")
+        .arg("tests/images/2.jpeg")
+        .arg("tests/images/output.jpeg");
     cmd.assert().success();
 }
